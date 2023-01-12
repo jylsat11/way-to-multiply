1925a965ad04d571801e6ecbc65e8178db868750
yyueun <sw3623@ajou.ac.kr>
2023-01-12 10:01:06 +0900

    feat: Develop division function

--- ethiopian.py
+++ ethiopian.py
@@ -3,3 +3,16 @@ def hello():
 
 if __name__ == '__main__':
     hello()
+
+
+
+#division
+
+def division_two(division_num:int):
+  division_list = [division_num]
+  while division_num!=1:
+    division_list.append(division_num//2)
+    division_num=division_num//2
+
+  return division_list
+
